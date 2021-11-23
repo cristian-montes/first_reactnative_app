02dfc97211f72316c0444d31ae0f82aea9004721
Cristian Montes <cmontesmujica@gmail.com>
2021-11-22 19:27:26 -0700

    added button and routing to components screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello Fellow</Text>;
+const HomeScreen = (props) => {
+ 
+  return (
+    <View>
+      <Text style={styles.text}>Hello Fellow</Text>
+      <Button
+        onPress={() => props.navigation.navigate('Components')}
+        title="Go to Components Demo"
+      />
+    </View>
+  )
 };
+
+
+
 
 const styles = StyleSheet.create({
   text: {
